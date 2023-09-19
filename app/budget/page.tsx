45d05bb3804ec2c4142f9d2405dc0cedf724db45
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-09-19 17:00:08 +0700

    budget page

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,3 +1,34 @@
+import ItemCard from "./ItemCard";
+import { FaLocationArrow } from "react-icons/fa";
+
 export default function Page() {
-  return <p>budget</p>;
+  return (
+    <div>
+      <div className="pb-16">
+        <div className="pb-8">
+          <hr className="border-black" />
+          <div className="flex items-center gap-2">
+            <FaLocationArrow size={20} />
+            <h1 className="text-3xl font-serif">VIETNAM</h1>
+          </div>
+        </div>
+        <div className="space-y-8">
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+        </div>
+      </div>
+      <div className="pb-16">
+        <div className="pb-8">
+          <hr className="border-black" />
+          <h1 className="text-3xl font-serif">THAILAND</h1>
+        </div>
+        <div className="space-y-8">
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+          <ItemCard name={"Ban mi"} price={10} type={"food"} />
+        </div>
+      </div>
+    </div>
+  );
 }
