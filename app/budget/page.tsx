8fbfc620fbac62082bfc357cb937765189722d79
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-09-23 19:05:40 +0700

    budget fix

--- app/budget/page.tsx
+++ app/budget/page.tsx
@@ -1,34 +1,12 @@
 import ItemCard from "./ItemCard";
-import { BiSolidMapPin } from "react-icons/bi";
+import budget from "./budget.json";
 
 export default function Page() {
   return (
-    <div>
-      <div className="pb-16">
-        <div className="pb-8">
-          <div className="flex items-center gap-2">
-            <BiSolidMapPin size={30} />
-            <h1 className="text-3xl">VIETNAM</h1>
-          </div>
-          <hr className="border-black" />
-        </div>
-        <div className="space-y-8">
-          <ItemCard name={"Ban mi"} price={10} type={"food"} rating={3} />
-          <ItemCard name={"Öl"} price={12} type={"drink"} rating={4} />
-          <ItemCard name={"Massage"} price={100} type={"activity"} rating={5} />
-        </div>
-      </div>
-      <div className="pb-16">
-        <div className="pb-8">
-          <hr className="border-black" />
-          <h1 className="text-3xl font-serif">THAILAND</h1>
-        </div>
-        <div className="space-y-8">
-          <ItemCard name={"Ban mi"} price={10} type={"food"} />
-          <ItemCard name={"Ban mi"} price={10} type={"food"} />
-          <ItemCard name={"Ban mi"} price={10} type={"food"} />
-        </div>
-      </div>
+    <div className="space-y-8">
+      {budget.map((item, index) => (
+        <ItemCard key={index} prices={item.prices} category={item.name} />
+      ))}
     </div>
   );
 }
