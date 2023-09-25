8b4d981559c97a47deb1bcfe06b6294c5971a460
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-09-25 17:43:45 +0700

    update image import

--- app/budget/sponsra/page.tsx
+++ app/budget/sponsra/page.tsx
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import swish from "@/public/assets/swish.png";
-import swishinfo from "@/public/assets/swishinfo.png";
-import swishbutton from "@/public/assets/swishbutton.png";
 
 export default function Page() {
   return (
     <div className="w-full h-full m-auto space-y-6 max-w-md">
-      <Image quality={100} src={swishinfo} alt={"Swish info"} />
-      <Image quality={100} src={swishbutton} alt={"Swish button"} />
+      <Image quality={100} src={"/assets/swishinfo.png"} alt={"Swish info"} />
+      <Image
+        quality={100}
+        src={"/assets/swishbutton.png"}
+        alt={"Swish button"}
+      />
       <p className="pt-2 text-center">
         Att skänka en öl eller kaffe är mycket enkelt. Alla bidrag besvaras
         personligen med ett inlägg på{" "}
