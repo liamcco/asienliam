a5f26274781684f541e4859b3e0692c11094bb93
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-09-25 17:30:02 +0700

    added infotext to sponsor

--- app/budget/sponsra/page.tsx
+++ app/budget/sponsra/page.tsx
@@ -5,6 +5,13 @@ export default function Page() {
   return (
     <div className="w-full h-full my-auto">
       <Image quality={100} src={swish} alt={"Swish"} />
+      <p className="pt-2 text-center">
+        Att skänka en öl eller kaffe är mycket enkelt. Alla bidrag besvaras
+        personligen med ett inlägg på{" "}
+        <a href="https://instagram.com/asienliam" className="font-bold">
+          @asienliam
+        </a>
+      </p>
     </div>
   );
 }
