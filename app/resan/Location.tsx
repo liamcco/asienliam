edc7eb96873f647960bfc47f682ba031a6e9935c
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-09-28 19:47:36 +0700

    added images and updated info

--- app/resan/Location.tsx
+++ app/resan/Location.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import { FaLocationArrow, FaMapPin } from "react-icons/fa";
 import Image from "next/image";
 
@@ -9,26 +12,29 @@ interface Props {
 
 export default function Location(props: Props) {
   const current = <p className="text-gray-600">CURRENTLY IN</p>;
+  const imageSize = 40;
+  const [imageBig, setImageBig] = useState(false);
 
   return (
     <div className="flex items-center">
       <div className="p-4">
         {props.duration ? <FaMapPin /> : <FaLocationArrow />}
       </div>
-      <div className="w-full flex justify-between">
+      <div className="w-full flex justify-between items-center">
         <div>
           <h1 className="font-bold">{props.city}</h1>
           {props.duration == "" ? current : <p>{props.duration}</p>}
         </div>
         {props.image && (
-          <div className="rounded-full overflow-hidden border-2">
+          <button className="transition rounded-full overflow-hidden border-2">
             <Image
               src={"/assets/photos/" + props.image}
               alt={"Photo from city"}
-              width={40}
-              height={40}
+              width={512}
+              height={512}
+              style={{ width: imageSize, height: imageSize }}
             />
-          </div>
+          </button>
         )}
       </div>
     </div>
