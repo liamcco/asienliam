293be1a3b4b3d3b7bf87742e4c2088526170c99c
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-10-02 21:31:48 +0700

    working animation

--- app/resan/Location.tsx
+++ app/resan/Location.tsx
@@ -1,42 +1,67 @@
-"use client";
-
-import { useState } from "react";
 import { FaLocationArrow, FaMapPin } from "react-icons/fa";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 interface Props {
   city: string;
   duration: string;
   image?: string;
+  grid: boolean;
+  handleClick?: () => void;
 }
 
 export default function Location(props: Props) {
   const current = <p className="text-gray-600">CURRENTLY IN</p>;
   const imageSize = 40;
-  const [imageBig, setImageBig] = useState(false);
 
   return (
-    <div className="flex items-center">
-      <div className="p-4">
-        {props.duration ? <FaMapPin /> : <FaLocationArrow />}
-      </div>
-      <div className="w-full flex justify-between items-center">
-        <div>
-          <h1 className="font-bold">{props.city}</h1>
-          {props.duration == "" ? current : <p>{props.duration}</p>}
+    <motion.div
+      className={
+        props.grid
+          ? "w-full flex items-center flex-col justify-end"
+          : "w-full flex items-center justify-between"
+      }
+    >
+      <motion.div
+        className={
+          props.grid ? "z-10 absolute bg-slate-300 rounded-md pr-6" : ""
+        }
+        layout
+      >
+        <div className="flex items-center">
+          <div className="p-4">
+            {props.duration ? <FaMapPin /> : <FaLocationArrow />}
+          </div>
+          <div>
+            <h1 className="font-bold">{props.city}</h1>
+            {props.duration == "" ? current : <p>{props.duration}</p>}
+          </div>
         </div>
-        {props.image && (
-          <button className="transition rounded-full overflow-hidden border-2">
-            <Image
-              src={"/assets/photos/" + props.image}
-              alt={"Photo from city"}
-              width={512}
-              height={512}
-              style={{ width: imageSize, height: imageSize }}
-            />
-          </button>
-        )}
-      </div>
-    </div>
+      </motion.div>
+      <motion.button
+        className={
+          props.grid
+            ? "rounded-full overflow-hidden border-2 z-0 mb-4"
+            : "rounded-full overflow-hidden border-2"
+        }
+        onClick={props.handleClick}
+        layout
+      >
+        <Image
+          src={
+            props.image
+              ? "/assets/photos/" + props.image
+              : "/assets/photos/fallback.jpg"
+          }
+          alt={"Photo from city"}
+          width={512}
+          height={512}
+          style={{
+            width: props.grid ? imageSize * 5 : imageSize,
+            height: props.grid ? imageSize * 5 : imageSize,
+          }}
+        />
+      </motion.button>
+    </motion.div>
   );
 }
--- app/resan/page.tsx
+++ app/resan/page.tsx
@@ -1,28 +1,35 @@
+"use client";
+
 import trip from "./trip.json";
 import Location from "./Location";
-import { addBorderChanges } from "./itinerary";
+import { useState } from "react";
 
 export default function Page() {
-  const initialItinerary = trip.map((item, index) => {
-    return (
-      <Location
-        city={item.city}
-        duration={item.duration}
-        key={index}
-        image={item.image}
-      />
-    );
-  });
-
-  let itinerary = addBorderChanges(trip, initialItinerary);
+  const [grid, setGrid] = useState(false);
 
   return (
     <div className="mt-8 w-full max-w-md mx-auto">
-      <div className="space-y-4">
-        {itinerary.map((item, index) => (
-          <div key={index}>{item}</div>
-        ))}
-      </div>
+      {trip.map((country, i) => (
+        <div key={i} className="space-y-4">
+          {country["cities"].map((city, j) => (
+            <Location
+              city={city.city}
+              duration={city.duration}
+              key={j}
+              image={city.image}
+              grid={grid}
+              handleClick={() => setGrid(!grid)}
+            />
+          ))}
+          {trip[i + 1] && (
+            <div className="pb-4">
+              <h3>{trip[i]["country"]}</h3>
+              <hr />
+              <h3>{trip[i + 1]["country"]}</h3>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
