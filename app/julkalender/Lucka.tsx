afdb4b7704910074e66ead9c556241fc38e56717
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-11-25 16:22:12 +1300

    basic layout complete

---
+++ app/julkalender/Lucka.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+
+interface Props {
+  index: number;
+}
+
+type State = "closed" | "open" | "today";
+
+export default function Lucka(props: Props) {
+  const today = new Date();
+  const luckDate = new Date("December " + props.index + ", 2023");
+
+  let state: State = "closed";
+  if (luckDate < today) {
+    state = "open";
+  }
+  if (luckDate.getDate() === today.getDate()) {
+    state = "today";
+  }
+
+  return (
+    <div className="rounded-lg bg-slate-300 text-center flex flex-col justify-center aspect-square">
+      <h1 className="text-3xl font-bold">{props.index}</h1>
+    </div>
+  );
+}
