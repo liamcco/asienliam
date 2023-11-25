b2c9c4d8c97d18bb9a2b96ca26af90a5d23ab107
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-11-25 13:39:47 +1300

    navbar updated

---
+++ app/julkalender/lucka/id/page.tsx
@@ -0,0 +1,8 @@
+export default function Lucka() {
+  return (
+    <div>
+      <h1>Lucka</h1>
+      <p>Detta är en lucka</p>
+    </div>
+  );
+}
--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,11 +1,11 @@
 export default function Navbar() {
   return (
-    <nav className="flex-none mb-8">
+    <nav className="mb-8">
       <div className="font-bold font-mono">
         <h1 className="text-4xl mb-4">
           <a href="/">@asienliam</a>
         </h1>
-        <ul className="flex justify-between">
+        <ul className="flex justify-between flex-wrap gap-x-8 gap-y-4">
           <li>
             <a href="/resan">Resan</a>
           </li>
@@ -14,6 +14,14 @@ export default function Navbar() {
           </li>
           <li>
             <a href="/budget">Budget</a>
+          </li>
+          <li className="mx-auto sm:ml-auto sm:mr-0">
+            <a
+              href="/julkalender"
+              className="p-2 bg-slate-200 border rounded-xl"
+            >
+              ✨ Liams Julkalender 2023 🎅🏻
+            </a>
           </li>
         </ul>
       </div>
