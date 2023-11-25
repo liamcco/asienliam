2ae78812bacd1cd5259a86d622bec05c9163927a
Liam Cotton <37960435+liamcco@users.noreply.github.com>
2023-11-25 18:44:52 +1300

    removed animation

--- app/planen/page.tsx
+++ app/planen/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import plan from "./plan.json";
-import { motion } from "framer-motion";
 
 export default function Page() {
   return (
@@ -14,22 +11,14 @@ export default function Page() {
             </h2>
             <div className="w-full max-w-md">
               {entry.cities.map((city, subindex) => (
-                <motion.div
+                <div
                   key={subindex}
                   className="flex gap-8 justify-between px-8 py-2 my-2 border rounded-md bg-slate-400"
                   style={{ opacity: Math.max(0.4, 1 - index / plan.length) }}
-                  initial={{ scale: 0, rotate: -15 }}
-                  animate={{ rotate: 0, scale: 1 }}
-                  transition={{
-                    type: "spring",
-                    stiffness: 260,
-                    damping: 20,
-                    delay: subindex / entry.cities.length,
-                  }}
                 >
                   <h3>{city.name}</h3>
                   <p>{city.date}</p>
-                </motion.div>
+                </div>
               ))}
             </div>
           </div>
